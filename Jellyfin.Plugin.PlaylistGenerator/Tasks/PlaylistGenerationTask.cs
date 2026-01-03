3a2d2b6d98591c4eace6a73148c8a75f7e95b6f4
Eeeeelias <elias.albrecht@zohomail.eu>
2026-01-03 23:12:11 +0100

    refactoring generation task

--- Jellyfin.Plugin.PlaylistGenerator/Tasks/PlaylistGenerationTask.cs
+++ Jellyfin.Plugin.PlaylistGenerator/Tasks/PlaylistGenerationTask.cs
@@ -6,6 +6,7 @@ using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Entities;
 
 using Jellyfin.Data.Enums;
+using Jellyfin.Database.Implementations.Entities;
 using MediaBrowser.Controller.Playlists;
 using Jellyfin.Plugin.PlaylistGenerator.Objects;
 using MediaBrowser.Controller;
@@ -37,6 +38,111 @@ public class PlaylistGenerationTask(ILibraryManager libraryManager,
     public string Description => "Generate a playlist based on previous listen data + similarity.";
     public string Category => "Library";
 
+    private List<ICollectionFolder?> GetLibraries(User currentUser)
+    {
+        // get all libraries and then filter for music libraries
+        var allFolders = _libraryManager.GetUserRootFolder()
+            .GetChildren(currentUser, true)
+            .OfType<Folder>()
+            .ToList();
+        
+        
+        var musicLibraries = allFolders.Select(folder => folder as ICollectionFolder)
+            .Where(collectionFolder => collectionFolder?.CollectionType == CollectionType.music).ToList();
+        
+        var selectedLibraries = musicLibraries
+            .Where(cf => Config.SelectedLibraryIds.Contains(cf!.Id))
+            .ToList();
+        
+        if (selectedLibraries.Count <= 0)
+        {
+         _logger.LogInformation($"No library found for user: {currentUser.Username}.");
+         return null;
+        }
+        _logger.LogInformation($"Generating playlist from libraries: {string.Join(", ", selectedLibraries.Select(l => l.Name))}");
+        return selectedLibraries;
+    }
+
+    private List<BaseItem> GetAllSongs(List<ICollectionFolder?> selectedLibraries, CancellationToken token)
+    {
+        var allAudio = new List<BaseItem>();
+        foreach (var library in selectedLibraries)
+        {
+            token.ThrowIfCancellationRequested();
+            if (library == null)
+            {
+                _logger.LogWarning("No library here, skipping.");
+                continue;
+            }
+            _logger.LogInformation($"Searching for songs in library: {library.Name}");
+            var songQuery = new InternalItemsQuery{
+                IncludeItemTypes = [BaseItemKind.Audio], 
+                ParentId = library.Id,
+                Recursive = true
+            };
+            var audioItems = _libraryManager.GetItemList(songQuery);
+            allAudio.AddRange(audioItems);
+        }
+
+        return allAudio;
+    }
+
+    private List<ScoredSong> BuildPlaylist(List<BaseItem> songs, User currentUser, CancellationToken token)
+    {
+        // first get all songs
+        var songList = new List<ScoredSong>();
+        
+        foreach (var song in songs)
+        {
+            token.ThrowIfCancellationRequested();
+            songList.Add(new ScoredSong(song, currentUser, _userDataManager, _libraryManager, _activityDatabase));
+        }
+
+        // initialise the Recommenders and get some recommendations based on our top
+        var experimentalFilter = Config.ExperimentalFilter;
+        Recommender playlistRecommender = new(_libraryManager, _userDataManager, _activityDatabase, Config.ExplorationCoefficient);
+        
+        List<ScoredSong> topSongs = [.. songList.OrderByDescending(song => song.Score).Take(20)];
+        var similarBySong = playlistRecommender.RecommendSimilar(topSongs, currentUser);
+        var similarByGenre = playlistRecommender.RecommendByGenre(topSongs, currentUser, experimentalFilter);
+        var similarByArtist = playlistRecommender.RecommendByArtist(topSongs, currentUser, experimentalFilter);
+        var favouriteSongs = playlistRecommender.RecommendByFavourite(topSongs, currentUser);
+
+        List<ScoredSong> allSongs = [..topSongs];
+        allSongs.AddRange(similarBySong);
+        allSongs.AddRange(similarByGenre);
+        allSongs.AddRange(similarByArtist);
+        allSongs.AddRange(favouriteSongs);
+        
+        // prune songs that are too short or have no ParentId 
+        allSongs = allSongs.Where(song => (song.Song.RunTimeTicks ?? 0 / TimeSpan.TicksPerSecond) >= Config.ExcludeTime 
+                                          && song.Song.ParentId != Guid.Empty).ToList();
+
+        _logger.LogInformation($"Highest score: {allSongs[0].Score} for song: {allSongs[0].Song.Name}");
+        var assembledPlaylist = PlaylistService.AssemblePlaylist(allSongs, Config.PlaylistDuration, 
+            playlistRecommender, currentUser);
+        assembledPlaylist = PlaylistService.GentleShuffle(assembledPlaylist, 10, true);
+        return assembledPlaylist;
+    }
+
+    private void HandleJellyPlaylist(List<ScoredSong> assembledPlaylist, User currentUser)
+    {
+        // check if playlist exists
+        var allPlaylists = _libraryManager.GetItemList(new InternalItemsQuery{IncludeItemTypes = 
+            [BaseItemKind.Playlist]});
+        PlaylistService playlistServer = new(_playlistManager, _libraryManager);
+
+        if (allPlaylists.Any(playlist => playlist.Name.Equals(Config.PlaylistName))) 
+        {
+            _logger.LogInformation($"Playlist {Config.PlaylistName} exists. Overwriting.");
+            playlistServer.RemovePlaylist(Config.PlaylistName);
+        }
+
+        // make the playlist
+        playlistServer.CreatePlaylist(Config.PlaylistName, currentUser, assembledPlaylist);
+    }
+    
+    
     public Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
@@ -62,45 +168,11 @@ public class PlaylistGenerationTask(ILibraryManager libraryManager,
             _logger.LogWarning($"User: {Config.PlaylistUserName} not found. Aborting.");
             return Task.CompletedTask;
         }
-        
-        // get all libraries and then filter for music libraries
-        var allFolders = _libraryManager.GetUserRootFolder()
-            .GetChildren(currentUser, true)
-            .OfType<Folder>()
-            .ToList();
-        
-        
-        var musicLibraries = allFolders.Select(folder => folder as ICollectionFolder)
-            .Where(collectionFolder => collectionFolder?.CollectionType == CollectionType.music).ToList();
-        
-        var selectedLibraries = musicLibraries
-            .Where(cf => Config.SelectedLibraryIds.Contains(cf!.Id))
-            .ToList();
-        
-        _logger.LogInformation($"Generating playlist from libraries: {string.Join(", ", selectedLibraries.Select(l => l.Name))}");
-        
-        // first get all songs
-        var songList = new List<ScoredSong>();
+
+        var selectedLibraries = GetLibraries(currentUser);
 
         // search for songs in the music libraries
-        var allAudio = new List<BaseItem>();
-        foreach (var library in selectedLibraries)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            if (library == null)
-            {
-                _logger.LogWarning("No library here, skipping.");
-                continue;
-            }
-            _logger.LogInformation($"Searching for songs in library: {library.Name}");
-            var songQuery = new InternalItemsQuery{
-                IncludeItemTypes = [BaseItemKind.Audio], 
-                ParentId = library.Id,
-                Recursive = true
-            };
-            var audioItems = _libraryManager.GetItemList(songQuery);
-            allAudio.AddRange(audioItems);
-        }
+        var allAudio = GetAllSongs(selectedLibraries, cancellationToken);
 
         if (allAudio.Count <= 0)
         {
@@ -109,7 +181,6 @@ public class PlaylistGenerationTask(ILibraryManager libraryManager,
         }
         
         // filter out theme songs and songs that are too short
-        _logger.LogInformation($"Found {allAudio.Count} songs, filtering out theme songs and short songs.");
         var noThemeSongs = allAudio.Where(song => song.IsThemeMedia == false).ToList();
         var songs = noThemeSongs.Where(song => (int)((long)(song.RunTimeTicks ?? 0) / TimeSpan.TicksPerSecond) > Config.ExcludeTime).ToList();
         
@@ -118,56 +189,13 @@ public class PlaylistGenerationTask(ILibraryManager libraryManager,
             _logger.LogWarning("No music found after filtering.");
             return Task.CompletedTask;
         }
-
-        _logger.LogInformation($"Found {songs.Count} songs");
-
-        foreach (var song in songs)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-
-            songList.Add(new ScoredSong(song, currentUser, _userDataManager, _libraryManager, _activityDatabase));
-        }
-
-        // initialise the Recommenders and get some recommendations based on our top
-        var experimentalFilter = Config.ExperimentalFilter;
-        PlaylistService playlistServer = new(_playlistManager, _libraryManager);
-        Recommender playlistRecommender = new(_libraryManager, _userDataManager, _activityDatabase, Config.ExplorationCoefficient);
+    
+        _logger.LogInformation($"Found {allAudio.Count} songs, filtering out theme songs and short songs...{songs.Count} remaining.");
+        var assembledPlaylist = BuildPlaylist(songs, currentUser, cancellationToken);
         
-        List<ScoredSong> topSongs = [.. songList.OrderByDescending(song => song.Score).Take(20)];
-        var similarBySong = playlistRecommender.RecommendSimilar(topSongs, currentUser);
-        var similarByGenre = playlistRecommender.RecommendByGenre(topSongs, currentUser, experimentalFilter);
-        var similarByArtist = playlistRecommender.RecommendByArtist(topSongs, currentUser, experimentalFilter);
-        var favouriteSongs = playlistRecommender.RecommendByFavourite(topSongs, currentUser);
-
-        List<ScoredSong> allSongs = [..topSongs];
-        allSongs.AddRange(similarBySong);
-        allSongs.AddRange(similarByGenre);
-        allSongs.AddRange(similarByArtist);
-        allSongs.AddRange(favouriteSongs);
-        
-        // prune songs that are too short or have no ParentId 
-        allSongs = allSongs.Where(song => (song.Song.RunTimeTicks ?? 0 / TimeSpan.TicksPerSecond) >= Config.ExcludeTime 
-                                          && song.Song.ParentId != Guid.Empty).ToList();
-
-        _logger.LogInformation($"Highest score: {allSongs[0].Score} for song: {allSongs[0].Song.Name}");
-        var assembledPlaylist = PlaylistService.AssemblePlaylist(allSongs, Config.PlaylistDuration, 
-            playlistRecommender, currentUser);
-        assembledPlaylist = PlaylistService.GentleShuffle(assembledPlaylist, 10, true);
-
-        // check if playlist exists
-        var allPlaylists = _libraryManager.GetItemList(new InternalItemsQuery{IncludeItemTypes = 
-            [BaseItemKind.Playlist]});
-
-        if (allPlaylists.Any(playlist => playlist.Name.Equals(Config.PlaylistName))) 
-        {
-            _logger.LogInformation($"Playlist {Config.PlaylistName} exists. Overwriting.");
-            playlistServer.RemovePlaylist(Config.PlaylistName);
-        }
-
-        // make the playlist
-        playlistServer.CreatePlaylist(Config.PlaylistName, currentUser, assembledPlaylist);
-
+        HandleJellyPlaylist(assembledPlaylist, currentUser);
         _logger.LogInformation($"Generated personal playlist for {currentUser.Username}.");
+        
         return Task.CompletedTask;
     }
 
